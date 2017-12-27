5b3fb8b4d7935bce735659f6ba24b8202cbd6861
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2017-12-26 21:48:47 -0800

    Added dkms installation

--- scripts/install_infra.sh
+++ scripts/install_infra.sh
@@ -3,28 +3,18 @@
 
 sudo bash -c 'cat <<EOF >/etc/apt/sources.list
 ###### Ubuntu Main Repos
-deb http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe
-multiverse
-deb-src http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe
-multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
 
 ###### Ubuntu Update Repos
-deb http://01.archive.ubuntu.com/ubuntu/ xenial-security main restricted
-universe multiverse
-deb http://01.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe
-multiverse
-deb http://01.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted
-universe multiverse
-deb http://01.archive.ubuntu.com/ubuntu/ xenial-backports main restricted
-universe multiverse
-deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-security main restricted
-universe multiverse
-deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-updates main restricted
-universe multiverse
-deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted
-universe multiverse
-deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-backports main restricted
-universe multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-backports main restricted universe multiverse
 EOF'
 
 
