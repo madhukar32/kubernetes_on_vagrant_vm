e1fe6eea94c69e29cb22aacd891883067c0082cf
Madhukar Nayakbomman <madhukarnayak@hotmail.com>
2017-12-26 21:48:47 -0800

    Added dkms installation

--- scripts/install_infra.sh
+++ scripts/install_infra.sh
@@ -1,28 +1,54 @@
 #!/bin/bash -eux
 #Add basic repo to sources.list
-cat <<EOF >/etc/apt/sources.list
-deb http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
-deb-src http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe multiverse
-EOF
-sudo apt-get update -y
+
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list
+###### Ubuntu Main Repos
+deb http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe
+multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial main restricted universe
+multiverse
+
+###### Ubuntu Update Repos
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-security main restricted
+universe multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-updates main restricted universe
+multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted
+universe multiverse
+deb http://01.archive.ubuntu.com/ubuntu/ xenial-backports main restricted
+universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-security main restricted
+universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-updates main restricted
+universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-proposed main restricted
+universe multiverse
+deb-src http://01.archive.ubuntu.com/ubuntu/ xenial-backports main restricted
+universe multiverse
+EOF'
+
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update
 sleep 2
 
 #Install dependency packages
-sudo apt-get install \
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install \
     apt-transport-https \
     ca-certificates \
     curl \
-    vim-common=2:7.4.1689-3ubuntu1 \
     vim \
     build-essentials \
     dkms \
-    software-properties-common -y --allow-downgrades
+    software-properties-common
 
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-echo 'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' >> /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install virtualbox-5.1
+echo 'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' | \ 
+	sudo tee -a /etc/apt/sources.list > /dev/null
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install virtualbox-5.1
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install dkms 
 
 export TMP_DIR=$(mktemp -d)
 wget https://releases.hashicorp.com/vagrant/1.9.8/vagrant_1.9.8_x86_64.deb -P ${TMP_DIR}
-dpkg -i ${TMP_DIR}/vagrant_1.9.8_x86_64.deb
+sudo dpkg -i ${TMP_DIR}/vagrant_1.9.8_x86_64.deb
